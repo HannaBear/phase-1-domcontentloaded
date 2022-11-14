aee1b01924adbd966431e6935eee8d8785472738
Hanna <marmalademarmoset@gmail.com>
2022-11-14 12:56:35 -0800

    commit tree b5a784c86e41c510d38c33247c07adc015022bb6

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+const domLoaded = document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+  document.getElementById('text')
+  text.textContent = "This is really cool!"  
+});
+
